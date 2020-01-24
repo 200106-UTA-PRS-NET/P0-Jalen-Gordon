2438d4229c8c6da607c0b8e269550424455a48d4
JalenG95 <48186864+JalenG95@users.noreply.github.com>
2020-01-24 09:07:13 -0600

    Add files via upload

---
+++ pizzabox.sql
@@ -0,0 +1,47 @@
+-- Create database "PizzaDb
+create database PizzaDb;
+go;
+use PizzaDb;
+go;
+-- Create Schema "Revature
+create schema Revature;
+go;
+
+-- Create Customer table 
+
+create table Revature.Customer(
+Username varchar(20) not null,
+Passw varchar(max) not null,
+Previousorder varchar(max) not null
+primary key(username)
+)
+
+-- Create Store Table
+create table Revature.Store(
+Storename varchar(20) not null,
+Storepassword varchar(20) not null
+primary key(Storename)
+)
+
+--Create CustomerOrder
+create table Revature.CustomerOrder(
+orderId int Identity(1,1) not null,
+Storename varchar(20) not null,
+Username varchar(20) not null,
+Price decimal(10,9),
+Pizza varchar(max) not null
+Primary Key(orderId)
+)
+
+-- CustomerOrder table holds foreign key (column name 'storename') from Store table
+alter table Revature.CustomerOrder
+add constraint FK_Store_Order_Name foreign key(storename) references Revature.Store(storename)
+
+-- CustomerOrder table holds foreign key (column name 'username) from Customer table
+alter table Revature.CustomerOrder
+add constraint FK_User_Order_Name foreign key(username) references Revature.Customer(username)
+
+
+--Select all data from table CustomerOrder
+select * from Revature.CustomerOrder
+
